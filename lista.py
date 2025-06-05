92cf862186118b45fb44515c9910461ce7ba7b33
Aliii666 <aliyhamy04@gmail.com>
2025-06-05 15:24:09 -0500

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,22 @@
+lista = [1,2,3,4]
+print(lista[0])
+print(lista[-1])
+print(lista[-2])
+print(lista[-3])
+
+#append - agregar un valor a la lista
+lista.append(5)
+#insert - inserta un elemento especifico y lugar especifico
+lista.insert(1,10)
+#remove - Elimina un elemento especifico
+lista.remove(4)
+#pop - Elimina un indice especifico  INDICE NUMERO DE LA POSICION 
+lista.pop(0)
+#clear
+lista.clear()
+
+
+
+
+
+print(lista)
